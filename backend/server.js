e05ad3d6bdbd36551d97d9f8a2e37dfed3996952
sonabaskaran <bsona2470@gmail.com>
2024-11-26 20:25:09 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended:false}));
 app.use(cors({
-   origin:["http://localhost:3000", "http://task-manager.com"]
+   origin:["http://localhost:3000", "http://task-manager.onrender.com"]
 }))
 
 app.use("/api/tasks",taskRoutes);
